bff7dc578963cc10874e345eeda31dec685234d7
saitarun135 <saitarun800@gmail.com>
2021-09-07 17:18:57 +0530

    refactor: getting id from the Trait

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -12,12 +12,12 @@ use Illuminate\Http\Request;
 class NotesController extends Controller
 {
     use AuthTrait;
+
    public function createNotes(Request $request){
        $note=new Notes();
        $note->title=$request->input('title');
        $note->body=$request->input('body');
-       $id=$this->getData();
-       $note->user_id = $id["sub"]; 
+       $note->user_id = $this->getData(); 
        $user_mail=User::where('id',$note->user_id)->value('email');
        $note->createdBy=$user_mail;
        $note->save();
@@ -40,8 +40,7 @@ class NotesController extends Controller
         return response()->json(['Alert'=>"email is not registered"]);
     }
     $Target_note=Notes::findOrFail($id); 
-    $id_fromTrait=$this->getData();
-    $check_id=$id_fromTrait["sub"];
+    $check_id=$this->getData();
     if($email==$verify){
         if($Target_note->user_id=$check_id ){
             $assignedValue=Notes::where('id',$id)->update(array('assignedTo'=>$email));
@@ -57,9 +56,8 @@ class NotesController extends Controller
    public function removeMailFromCollabarator(Request $request){
     $id=$request->input('id');
     $email=$request->input('email');
-    $Target_note=Notes::findOrFail($id);
-    $id_getter = $this->getData();   
-    $check_id=$id_getter["sub"];
+    $Target_note=Notes::findOrFail($id);  
+    $check_id=$this->getData();
     if($Target_note->user_id=$check_id ){
         $value_removed=Notes::where('id',$id)->update(array('assignedTo'=>null));
         $collabaratorRemoved=collabarator::where('note_id',$id)->where('collab_mails',$email)->delete();
@@ -68,22 +66,19 @@ class NotesController extends Controller
    }
 
    public function getNotes(){
-    $notes=Notes::all();
-    $id = $this->getData();        
-    $check_id=$id["sub"];
+    $notes=Notes::all();       
+    $check_id=$this->getData();
     $Normal_notes=User::find($notes->user_id=$check_id )->noteses;
     $email=User::where('id',$check_id)->value('email');
     $assignedMail=$email;  
-    
+
     if($notes->user_id=$check_id){
         $collabaratorNotes=Notes::select("notes.id","notes.title","notes.body","notes.assignedTo","notes.createdBy")
         ->leftJoin("collabarator","collabarator.collab_mails","=","notes.assignedTo")->distinct()
         ->where("notes.assignedTo","=","$assignedMail")
-        ->get();
-        
+        ->get();       
     $SharingMails=collabarator::select("collabarator.collab_mails")
-        ->leftJoin("notes","notes.id","=","collabarator.note_id")->distinct()->get();  
-        
+        ->leftJoin("notes","notes.id","=","collabarator.note_id")->distinct()->get();          
     return response()->json(['notes'=>$Normal_notes,'collabarator'=>$collabaratorNotes,'sharingEmails'=>$SharingMails]);
      }
     }
